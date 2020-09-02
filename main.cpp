b1fba8ee0df80304d7da9e0b0809c95215ad544c
azurec <alienneox@gmail.com>
2020-09-02 18:14:53 +0800

    complete demo coding

--- main.cpp
+++ main.cpp
@@ -39,11 +39,11 @@ static ec_slave_config_state_t sc_state = {};
 // PDO
 static uint8_t *domain1_pd = NULL;
 
-#define SynapticonSlave 0,0;
-#define Synapticon  0x000022D2，0x00000201// vendor id + product id
+#define SynapticonSlave 0,0
+#define Synapticon  0x000022D2,0x00000201// vendor id + product id
 
 // Offsets for PDO entries
-static struct offset
+static struct
 {
     unsigned int operation_mode;
     unsigned int ctrl_word;
@@ -51,7 +51,7 @@ static struct offset
     unsigned int status_word;
     unsigned int current_position;
     unsigned int current_velocity;
-};
+}offset;
 
 const static ec_pdo_entry_reg_t domain1_reg[] = {
     {SynapticonSlave, Synapticon, 0x6040, 0, &offset.ctrl_word},
@@ -61,7 +61,7 @@ const static ec_pdo_entry_reg_t domain1_reg[] = {
     {SynapticonSlave, Synapticon, 0x6064, 0, &offset.current_position},
     {SynapticonSlave, Synapticon, 0x606C, 0, &offset.current_velocity},
     {}
-}
+};
 
 // config PDOs
 static ec_pdo_entry_info_t device_pdo_entries[] = {
@@ -74,6 +74,13 @@ static ec_pdo_entry_info_t device_pdo_entries[] = {
     {0x6041, 0x00, 16}, // Status word
     {0x606C, 0x00, 32}, // actual velocity
     {0x6064, 0x00, 32}  // actual position
+};
+
+static ec_pdo_info_t device_pdos[] = {
+        /* RxPDO */
+        {0x1600,3,device_pdo_entries + 0},
+        /* TxPDO */
+        {0x1A00,3,device_pdo_entries + 3}
 };
 
 static ec_sync_info_t device_syncs[] = {
@@ -89,12 +96,133 @@ static ec_sync_info_t device_syncs[] = {
 
 void check_domain1_state(void){
     ec_domain_state_t ds;
-    ecrt_domain_state(domain1, &ds);
+    ecrt_domain_state(domain1, &ds);  // read state of the domain
+    if (ds.working_counter != domain1_state.working_counter)
+        std::cout << "Domain1 : Wc " << ds.working_counter << std::endl;
+    if (ds.wc_state != domain1_state.wc_state)
+        std::cout << "Domain1 : state " << ds.wc_state << std::endl;
+    domain1_state = ds;
+}
 
+void check_master_state(void){
+    ec_master_state_t ms;
+    ecrt_master_state(master,&ms); // read state of the master
+    if(ms.slaves_responding != master_state.slaves_responding)
+        std::cout << ms.slaves_responding << " slave(s)." << std::endl;
+    if(ms.al_states != master_state.al_states)
+        std::cout << "AL state : " << ms.al_states << std::endl; // applied layout
+    if(ms.link_up != master_state.link_up)
+        printf("Link is %s.\n", ms.link_up ? "up":"down");
+    master_state = ms;
+}
+
+void check_slave_config_states(void){
+    ec_slave_config_state_t s;
+    ecrt_slave_config_state(sc,&s);
+    if (s.al_state != sc_state.al_state)
+        printf("Slave: State 0x%02X.\n",s.al_state);
+    if(s.online != sc_state.online)
+        printf("Slave: %s.\n",s.online ? "online" : "offline");
+    if(s.operational != sc_state.operational)
+        printf("slave: %soperational.\n",s.operational ? "":"Not");
+    sc_state = s;
+}
+
+void cyclic_task(){
+    static uint16_t command = 0x004F; // used for judge status word
+    uint16_t  status;
+    // RxP data
+    ecrt_master_receive(master);
+    ecrt_domain_process(domain1);
+    // check process data state
+    check_domain1_state();
+
+    check_master_state();
+
+    check_slave_config_states();
+
+    // read state
+    status = EC_READ_U16((domain1_pd + offset.status_word));
+
+    if((status & command) == 0x0040){
+        EC_WRITE_U16(domain1_pd+offset.ctrl_word,0x0006);
+        EC_WRITE_S8(domain1_pd + offset.operation_mode, PROFILE_VELOCITY);
+        command = 0x006F;
+    }
+
+    else if ((status & command) == 0x0021)
+    {
+        EC_WRITE_U16(domain1_pd+offset.ctrl_word,0x000f);
+        command = 0x006F;
+    }
+    else if ((status & command) == 0x0027)
+    {
+        EC_WRITE_S32(domain1_pd + offset.target_velocity, TARGET_VELOCITY);
+        EC_WRITE_U16(domain1_pd + offset.ctrl_word, 0x001f);
+    }
+
+    ecrt_domain_queue(domain1);
+    ecrt_master_send(master);
 }
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Requesting master ... " << std::endl;
+    master = ecrt_request_master(0);
+    if(!master)
+        exit(EXIT_FAILURE);
+
+    domain1 = ecrt_master_create_domain(master);
+    if(!domain1)
+        exit(EXIT_FAILURE);
+
+    if(!(sc = ecrt_master_slave_config(master,SynapticonSlave,Synapticon)))
+    {
+        std::cout << "Failed to get slave configuration. " << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    if (ecrt_slave_config_pdos(sc,EC_END,device_syncs))
+    {
+        std::cout << "Failed to config slave PDOs" << std::endl;
+        exit(EXIT_FAILURE);
+    } else
+        std::cout << "Success to configuring slave PDOs" << std::endl;
+
+    if (ecrt_domain_reg_pdo_entry_list(domain1,domain1_reg))
+    {
+        std::cout << "PDO entry registration failed." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Activating master... " << std::endl;
+
+    if(ecrt_master_activate(master))
+        exit((EXIT_FAILURE));
+    else
+        std::cout << "Master activated!" << std::endl;
+
+    if(!(domain1_pd = ecrt_domain_data(domain1)))
+        exit(EXIT_FAILURE);
+
+    std::cout << "It is working now." << std::endl;
+
+    while(1)
+    {
+        usleep(100000/TASK_FREQUENCY);
+        cyclic_task();
+    }
+
+    return EXIT_SUCCESS
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
